3825e8e76e328e02cd6a4f579e729579468004fe
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-06-09 03:26:08 -0300

    Inserindo elementos básicos do HTML e implementndor as funções no cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,58 @@
 const cipher = {
-  // ...
+    encode: (string, offset) => {
+    offset = offset % 26;
+    let encryptedText = "";
+    let encryptedCode;
+
+    for(let i=0; i<string.length; i++) {
+      if (string.charCodeAt(i) >=65 && string.charCodeAt(i) <= 90) {
+        encryptedCode = (((string.charCodeAt(i) - 65) + offset) % 26) + 65;
+      }
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        encryptedCode = (((string.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      else if (string.charCodeAt(i) === 32) {
+        encryptedCode = 32;
+      }
+      else {
+        encryptedCode += string.charCodeAt(i);
+      }
+      encryptedText += String.fromCharCode(encryptedCode);
+    }
+    return encryptedText;
+  },
+
+  decode: (string, offset) => {
+    offset = offset % 26;
+    let decryptedText = "";
+    let decryptedCode;
+
+    for(let i=0; i<string.length; i++) {
+      if (string.charCodeAt(i) >=65 && string.charCodeAt(i) <= 90) {
+        decryptedCode = (((string.charCodeAt(i) - 65) - offset) % 26) + 65;
+      }
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        decryptedCode = (((string.charCodeAt(i) - 97) - offset) % 26) + 97;
+      }
+      else if (string.charCodeAt(i) === 32) {
+        decryptedCode = 32;
+      }
+      else {
+        decryptedCode += string.charCodeAt(i);
+      }
+      decryptedText += String.fromCharCode(decryptedCode);
+    }
+    return decryptedText;
+  }
+
+    // Alternativa para a função cipher.decode()
+    // decode: (string, offset) => {
+    // let decryptedText = "";
+
+    // offset = (26 - offset) % 26;
+    // decryptedText = cipher.encode(string, offset)
+    // return decryptedText;
+    //}
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
+const encodeButton = document.querySelector('#encode_button');
+let inputEncode = document.querySelector('#input_encode').value;
+let offsetEncode = document.querySelector('#offset_encode').value;
+//let encryptedText = document.querySelector('#encrypted_text').value;
+
+const decodeButton = document.querySelector('#decode_button');
+let inputDecode = document.querySelector('#input_decode').value;
+let offsetDecode = document.querySelector('#offset_decode').value;
+//let decryptedText = document.querySelector('#decrypted_text').value;
+
+encodeButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    cipher.encode(inputEncode, offsetEncode)
+})
+
+decodeButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    cipher.decode(inputDecode, offsetDecode)
+})
+
+
 console.log(cipher);
+console.log(cipher.encode("Patricia Barnabe", 3))
+console.log(cipher.decode("Sdwulfld Eduqdeh", 3))
+console.log(cipher.encode("Patricia Barnabe", 33))
+console.log(cipher.decode("Whaypjph Ihyuhil", 33))
+console.log(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33))
+console.log(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33))
+console.log(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33))
+console.log(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33))
